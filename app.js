d6f947d33c40ff37d7b5c7e71f77de5006ca1c64
tallspider <gqj200942@yahoo.ca>
2019-03-09 18:00:03 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = angular.module('app', []);
 
+function download(text, name, type) {
+  var a = document.getElementById("a");
+  var string = document.getElementById('streetNumber') + document.getElementById('streetName') + document.getElementById('City') + document.getElementById('Province');
+  var message = document.getElementById('message');
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+}
+
+
 app.controller('MainController', function ($scope, $http){
    $http({
       method: 'GET',
@@ -15,6 +25,23 @@ app.controller('MainController', function ($scope, $http){
    $scope.streetname = "";
    $scope.city = "";
    $scope.province = "";
-   $scope.reload = $route.reload();
+   $scope.tempaddress = "hi";
+
+   $scope.address = function(){
+     $scope.streetname = $scope.streetname.replace(' ', '+');
+     return $scope.streetnumber.toString() + '+' + $scope.streetname + '+' + $scope.city + '+' + $scope.province;
+   }
+
+   $scope.reviewProgram = "";
+   $scope.reviewDescript = "";
+
 }
 );
+
+// $("#save-btn").click(function(){
+//   var string = document.getElementById('streetNumber') + document.getElementById('streetName') + document.getElementById('City') + document.getElementById('Province');
+//   var message = document.getElementById('message');
+//   console.log(string + "\n" + message);
+//   var blobl = new Blob([string + "\n" + message], {type: "text/plain;charset-utf-8"});
+//   saveAs(blobl, "user-input.txt");
+// });
